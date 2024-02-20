772822f9a98aed582acb706939af39ca22d4b566
vusal <vusal0703@gmail.com>
2024-02-20 12:20:36 +0400

    yarisi hazir

--- js/script.js
+++ js/script.js
@@ -9,3 +9,32 @@ menuButton.addEventListener('click', function(){
 closeButton.addEventListener('click', function(){
     mobileMenu.classList.remove('active');
 });
+
+
+const projectButtons = document.querySelectorAll('.project-button');
+const projectCards = document.querySelectorAll('.project-card');
+projectButtons[0].classList.add('active');
+let firstButtonId = projectButtons[0].getAttribute('data-id');
+let firstGroupCards = document.querySelectorAll(`.project-card[data-group-id="${firstButtonId}"]`);
+
+for(let i = 0; i < firstGroupCards.length; i++){
+    firstGroupCards[i].classList.remove('d-none');
+}
+
+
+projectButtons.forEach((btn) => {
+    btn.addEventListener('click', function(){
+        const id = btn.getAttribute('data-id');
+        const cards = document.querySelectorAll(`.project-card[data-group-id="${id}"]`);
+        for(let i = 0; i < projectButtons.length; i++){
+            projectButtons[i].classList.remove('active');
+        }
+        for(let i = 0; i < projectCards.length; i++){
+            projectCards[i].classList.add('d-none');
+        }
+        for(let i = 0; i < cards.length; i++){
+            cards[i].classList.remove('d-none');
+        }
+        btn.classList.add('active');
+    });
+})
